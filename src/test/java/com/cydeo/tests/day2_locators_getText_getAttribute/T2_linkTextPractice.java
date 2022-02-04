905725c2bfa3339848cd2644c74734cc0dd75e4d
Alena <leksax@gmail.com>
2022-02-04 11:34:28 -0500

    Selenium day2  task2 of work

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_linkTextPractice.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_linkTextPractice {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //to maximize window
+        driver.manage().window().maximize();
+
+
+
+
+
+
+    }
+}
